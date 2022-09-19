7d4f28e5862376fb63e429298901343bfa2ebb57
prtmadty95 <aditya1995.jr@gmail.com>
2022-09-19 23:24:51 +0700

    Add function promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,34 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+let penontonMarah = 0;
+let penontonTidakMarah = 0;
+const promiseOutput = emosi => {
+  if (emosi === 'marah') {
+    return promiseTheaterIXX().then((nilai) => {
+      nilai.forEach((array) => {
+        if (array.hasil === 'marah') {
+          penontonMarah++;
+        } else if (array.hasil === 'tidak marah') {
+          penontonTidakMarah++;
+        } 
+      });
+      return penontonMarah
+    });
+  }
+  if (emosi === 'tidak marah') {
+    return promiseTheaterVGC().then((nilai) => {
+      nilai.forEach((array) => {
+        if (array.hasil === 'tidak marah') {
+          penontonTidakMarah++;
+        } else if (array.hasil === 'marah') {
+          penontonMarah++;
+        } 
+      });
+      return penontonMarah
+    });
+  }
+}
 
 module.exports = {
   promiseOutput,
